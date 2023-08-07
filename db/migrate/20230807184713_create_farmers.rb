6c9e2c81a2608fc0370d37061c7997a90039425e
Sergio Saavedra <sergio.saavedra@ug.uchile.cl>
2023-08-07 14:47:46 -0400

    add Farmer model

---
+++ db/migrate/20230807184713_create_farmers.rb
@@ -0,0 +1,10 @@
+class CreateFarmers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :farmers do |t|
+      t.string :name
+      t.string :age
+
+      t.timestamps
+    end
+  end
+end
